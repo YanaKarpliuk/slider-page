def366938cbc841d9553aec3577cf1c23b82254f
Yana Karpliuk <yanakarpliuk99@gmail.com>
2023-02-02 15:28:19 +0200

    removed script from html

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-import Glide, { Controls, Autoplay } from "@glidejs/glide/dist/glide.modular.esm";
+import Glide, {
+  Controls,
+  Autoplay,
+} from "@glidejs/glide/dist/glide.modular.esm";
 
 new Glide(".glide", {
   type: "carousel",
   autoplay: 4000,
   hoverpause: false,
-  gap: 0
+  gap: 0,
 }).mount({ Controls, Autoplay });
